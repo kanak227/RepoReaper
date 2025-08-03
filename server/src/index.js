9f5a5c15db6743bd5dd0a292b041539d64244e1e
kanak227 <kanakverma325@gmail.com>
2025-08-03 10:38:02 +0530

    fixes in cors

--- server/src/index.js
+++ server/src/index.js
@@ -27,7 +27,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production', 
+    secure: true, 
     httpOnly: true,
     sameSite: 'lax', 
     maxAge: 1000 * 60 * 60
