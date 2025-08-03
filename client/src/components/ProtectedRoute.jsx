bc8f01ad8cad758daea08a5c46845d8f3ee1c8e9
kanak227 <kanakverma325@gmail.com>
2025-08-03 21:56:19 +0530

    changed protected route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,12 +1,20 @@
 import { useAuthStore } from '../store/auth.store';
 import { Navigate } from 'react-router-dom';
+import Loader from './Loader';
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuthStore();
+  const { isAuthenticated, isLoading } = useAuthStore();
+
+  if (isLoading) {
+    return <Loader/>; // or a loading spinner — prevents premature redirect
+  }
+
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
+
   return children;
 };
 
 export default ProtectedRoute;
+
