3a711c01055a576cb34eea9fff74036005200f25
kanak227 <kanakverma325@gmail.com>
2025-08-03 10:33:04 +0530

    fixes in cors

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 // Configure CORS to work in both development and production
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'http://localhost:3000'], 
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'http://localhost:3000' , 'https://reporeaper-75wc.onrender.com/'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
