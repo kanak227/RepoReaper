dcfc5dfb2028de77dea6128fd8261b97027c2a85
Ayushi Srivastava <ayushisrivastava162006@gmail.com>
2026-06-23 19:02:45 +0530

    fix: address CodeRabbit review comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect,
+   useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
